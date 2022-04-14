8873099bc22510b57128cbadcd4b6810f4b7ab9d
jitunayak <jitunayak715@gmail.com>
2022-04-15 01:00:19 +0530

    added comments

--- find_palindrome.js
+++ find_palindrome.js
@@ -1,12 +1,15 @@
 function isPalindrome(input) {
   const len = String(input).length;
 
-  if (y < 0) {
+  if (input < 0) {
+    // negative numbers are not palindromes
     return false;
   }
-  const x = Array.from(String(input));
+  const x = Array.from(String(input)); // convert to char array
+
   for (let i = 0; i < len / 2; i++) {
     if (x[i] !== x[len - 1 - i]) {
+      // compare first and last char
       return false;
     }
   }
@@ -14,3 +17,4 @@ function isPalindrome(input) {
 }
 
 console.log(isPalindrome(121)); // true
+console.log(isPalindrome(-121)); // false
